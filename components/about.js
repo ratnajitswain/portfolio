5465312b4a8b662113fc9fff17f5606ba805c587
Ratnajit Swain <ratnajitswain@gmail.com>
2024-02-08 11:47:47 +0530

    update

--- components/about.js
+++ components/about.js
@@ -5,7 +5,7 @@ function About() {
         </h2>
         <p className="text-base mt-10 leading-loose text-gray-400">
             Experienced Full Stack Developer with 3 years of expertise in ReactJS, NodeJS, NextJS, ExpressJS, and LoopBack. Led the development of Shabd.in, a book publishing and reading platform with 200,000 monthly active users. Specializes in scalability optimization, localization, and backend performance enhancements. Proven track record in reducing load times, implementing MongoDB indexes, caching mechanisms, and advanced features like a robust coupon system. Committed to delivering innovative solutions for superior user experience and system efficiency.
-            write one line tag line for my portfolio
+            
         </p>
     </div>);
 }
