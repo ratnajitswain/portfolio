b5a4601b57a4526c1746e1e9e682dc7ee720427b
Ratnajit Swain <ratnajitswain@gmail.com>
2024-09-14 20:24:39 +0530

    chatbot

--- components/chat.js
+++ components/chat.js
@@ -1,6 +1,7 @@
 "use client"
 import React, { useState, useRef, useEffect } from 'react';
 import { marked } from 'marked';
+import me from "@/assets/images/me1.jpg";
 const initialAI = { role: 'ai', content: "Hello! I'm Ratnajit. Feel free to ask me anything about my skills, experience, or projects as a Full Stack Web Developer." }
 
 const ChatBot = () => {
@@ -115,9 +116,8 @@ const ChatBot = () => {
     };
 
     const clearHistory = () => {
-        const initialMessage = { role: 'ai', content: "Hello! I'm Ratnajit's AI assistant. Feel free to ask me anything about his skills, experience, or projects as a Full Stack Web Developer." };
-        setMessages([initialMessage]);
-        localStorage.setItem('chatHistory', JSON.stringify([initialMessage]));
+        setMessages([initialAI]);
+        localStorage.setItem('chatHistory', JSON.stringify([initialAI]));
     };
 
     return (
@@ -161,8 +161,8 @@ const ChatBot = () => {
                     className="fixed bottom-[calc(4rem+1.5rem)] left-0  md:left-[unset] right-0  md:mr-4 p-6 rounded-lg border border-gray-600 w-[440px] h-[634px] text-white"
                 >
                     <div className="flex flex-col space-y-1.5 pb-6">
-                        <h2 className="font-semibold text-lg tracking-tight">Chat with Ratnajit's AI Assistant</h2>
-                        <p className="text-sm text-gray-300 leading-3">Ask about Ratnajit's skills, projects, and experience</p>
+                        <h2 className="font-semibold text-lg tracking-tight">Chat with me</h2>
+                        <p className="text-sm text-gray-300 leading-3">Ask about my skills, projects, and experience</p>
                         <button
                             onClick={clearHistory}
                             className="text-xs text-gray-400 hover:text-white transition-colors duration-200"
@@ -176,22 +176,25 @@ const ChatBot = () => {
                                 <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
                                     <div className={`rounded-full ${message.role === 'ai' ? 'bg-gray-600 border-gray-500' : 'bg-blue-500 border-blue-400'} border p-1`}>
                                         {message.role === 'ai' ? (
-                                            <svg
-                                                stroke="none"
-                                                fill="white"
-                                                strokeWidth="1.5"
-                                                viewBox="0 0 24 24"
-                                                aria-hidden="true"
-                                                height="20"
-                                                width="20"
-                                                xmlns="http://www.w3.org/2000/svg"
-                                            >
-                                                <path
-                                                    strokeLinecap="round"
-                                                    strokeLinejoin="round"
-                                                    d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                                                ></path>
-                                            </svg>
+                                            // <svg
+                                            //     stroke="none"
+                                            //     fill="white"
+                                            //     strokeWidth="1.5"
+                                            //     viewBox="0 0 24 24"
+                                            //     aria-hidden="true"
+                                            //     height="20"
+                                            //     width="20"
+                                            //     xmlns="http://www.w3.org/2000/svg"
+                                            // >
+                                            //     <path
+                                            //         strokeLinecap="round"
+                                            //         strokeLinejoin="round"
+                                            //         d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
+                                            //     ></path>
+                                            // </svg>
+                                            <Image
+                                            className="h-5 w-5 object-cover" 
+                                            src={me}></Image>
                                         ) : (
                                             <svg stroke="none" fill="white" strokeWidth="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
