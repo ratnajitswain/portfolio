3c6987351998c23ba08480d6bea3532c7ca41e74
Ratnajit Swain <ratnajitswain@gmail.com>
2024-09-14 22:28:19 +0530

    prompt update

--- components/chat.js
+++ components/chat.js
@@ -3,6 +3,7 @@ import React, { useState, useRef, useEffect } from 'react';
 import { marked } from 'marked';
 import me from "@/assets/images/me2.jpg";
 import Image from 'next/image';
+
 const initialAI = { role: 'ai', content: "Hello! I'm Ratnajit. Feel free to ask me anything about my skills, experience, or projects as a Full Stack Web Developer." }
 
 const ChatBot = () => {
@@ -14,18 +15,20 @@ const ChatBot = () => {
     const chatWindowRef = useRef(null);
     const chatButtonRef = useRef(null);
     const messagesEndRef = useRef(null);
+
     useEffect(() => {
         try {
             const savedMessages = localStorage.getItem('chatHistory');
             if (savedMessages) {
                 setMessages(JSON.parse(savedMessages));
-            }else{
+            } else {
                 setMessages([initialAI]);
             }
         } catch (error) {
             setMessages([initialAI]);
         }
     }, []);
+
     useEffect(() => {
         localStorage.setItem('chatHistory', JSON.stringify(messages));
     }, [messages]);
@@ -197,29 +200,13 @@ const ChatBot = () => {
                                 <span className="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8">
                                     <div className={`rounded-full ${message.role === 'ai' ? 'bg-gray-600 border-gray-500' : 'bg-blue-500 border-blue-400'} border p-1`}>
                                         {message.role === 'ai' ? (
-                                            // <svg
-                                            //     stroke="none"
-                                            //     fill="white"
-                                            //     strokeWidth="1.5"
-                                            //     viewBox="0 0 24 24"
-                                            //     aria-hidden="true"
-                                            //     height="20"
-                                            //     width="20"
-                                            //     xmlns="http://www.w3.org/2000/svg"
-                                            // >
-                                            //     <path
-                                            //         strokeLinecap="round"
-                                            //         strokeLinejoin="round"
-                                            //         d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-                                            //     ></path>
-                                            // </svg>
                                             <div className="w-full h-full overflow-hidden rounded-full">
                                             <Image
                                                 className="object-cover w-full h-full" 
                                                 alt="Ratnajit Swain"
                                                 src={me}
                                             />
-                                        </div>
+                                            </div>
                                         ) : (
                                             <svg stroke="none" fill="white" strokeWidth="0" viewBox="0 0 16 16" height="20" width="20" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0Zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4Zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10Z"></path>
