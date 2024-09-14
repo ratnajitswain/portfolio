1417b580ef04cc55cd3000b37fc33efac5f47e31
Ratnajit Swain <ratnajitswain@gmail.com>
2024-09-14 19:43:20 +0530

    fixing envirnment vars

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,9 @@ const nextConfig = {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
+      },
+      env:{
+        GOOGLE_AI_API_KEY:process.env.GOOGLE_AI_API_KEY
       }
 };
 
